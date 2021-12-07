40fe76b63b5f5c17c92d0b43071f88057b2350f9
yunda1970 <95624280+yunda1970@users.noreply.github.com>
2021-12-07 19:01:08 +0700

    Create Algoritma Brute Force_Menggambar Garis_Klp5.py

---
+++ Algoritma Brute Force_Menggambar Garis_Klp5.py
@@ -0,0 +1,24 @@
+# Menginisialisasikan kordinat (x1,y1) dan kordinat (x2,y2)
+# Menghitung variabel N (banyaknya iterasi yang dilakukan apabila x1 tidak sama dengan x2 atau y1 tidak sama dengan y2)
+# Menjadikan x dan y sebagai titik awal
+# Membuat pengkondisian dengan beberapa syarat :
+
+# membuat kondisi dengan beberapa syarat :
+
+# 1) Jika x1 = x2 (garis vertikal), maka :
+#    a. y = y + 1 dan x tetap
+#    b. gambar titik kordinat (x,y)
+#    c. tampilkan grafik (selesai)
+
+# 2) Jika y1 = y2 (garis horizontal), maka :
+#    a. x = x + 1 dan y tetap
+#    b. gambar titik kordinat (x,y)
+#    c. tampilkan grafik (selesai)
+
+# 3) Jika 2 kondisi di atas salah, maka :
+#    a. hitung kemiringan garis dengan m = (y2 - y1) / (x2 - x1) 
+#    b. iterasi diulang sebanyak N
+#    c. y = m ( x - x1 ) + y1
+#    d. lakukan pembulatan pada y
+#    e. gambar titik (x, y(pembulatan))
+#    f. x = x + 1
